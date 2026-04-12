9c3f88152cc732aedaa21f55d885d912d9bc50dc
MCMarich <mcmarich@uci.edu>
2026-04-12 16:38:20 -0700

    Added The mixing logic, the empty glass, and making the El Diablo

---
+++ Bar Dwellers/Assets/Scripts/EmptyGlass.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Unity.VisualScripting.FullSerializer;
+using System.Net.Security;
+
+public class EmptyGlass : MonoBehaviour, IDropHandler
+{
+    public List<Recipe> recipeBook;
+    public Image liquidImage;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Mixer mixer = eventData.pointerDrag.GetComponent<Mixer>();
+
+        if(mixer != null && mixer.isMixed)
+        {
+            CreateDrink(mixer.contents);
+
+            mixer.contents.Clear();
+            mixer.isMixed = false;
+        }
+    }
+    void CreateDrink(List<DrinkType> ingredients)
+    {
+        foreach (Recipe recipe in recipeBook)
+        {
+            if (ingredients.Count == recipe.ingredients.Count)
+            {
+                bool match = true;
+                foreach (var item in recipe.ingredients)
+                {
+                    if (!ingredients.Contains(item)) match = false;
+                }
+                if (match)
+                {
+
+                }
+            }
+        }
+        Debug.Log("Did not create drink");
+    }
+    
+    void Serve(Recipe recipe)
+    {
+        Debug.Log("Served: " + recipe.DrinkName);
+        liquidImage.color = recipe.drinkColor;
+        liquidImage.gameObject.SetActive(true);
+    }
+    
+}
--- Bar Dwellers/Assets/Scripts/Mixer.cs
+++ Bar Dwellers/Assets/Scripts/Mixer.cs
@@ -1,49 +1,116 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 
-public class Mixer : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+public class Mixer : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler, IDropHandler
 {
+    [Header("Ingredients")]
     public List<DrinkType> contents = new List<DrinkType>();
-    public int MaxDrinks = 2;
+    public int maxCapacity = 2;
+    public bool isMixed = false;
 
     [Header("Shaking Settings")]
-    public float shakeRequired = 500f;
-
+    public float shakeRequired = 1000f; 
     private float currentShakeProgress = 0f;
-    private bool isMouseOver = false;
-    private Vector3 lastMousePos;
+    public Slider shakeProgressBar;
+    private Vector2 lastMousePos;
 
-    [Header("States")]
-    public bool isMixed = false;
+    [Header("Movement")]
+    [SerializeField] private Canvas canvas;
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+    private Vector2 originalPosition;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        originalPosition = rectTransform.anchoredPosition;
+    }
+
+    // This happens the moment you click to drag
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        lastMousePos = Mouse.current.position.ReadValue();
+        canvasGroup.alpha = .7f;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    // This runs every frame you move the mouse while clicking
+    public void OnDrag(PointerEventData eventData)
+    {
+        // LOGIC 1: If it's full but NOT mixed, we SHAKE
+        if (contents.Count >= maxCapacity && !isMixed)
+        {
+            HandleShakeLogic();
+        }
+        // LOGIC 2: If it IS mixed, we MOVE it to the glass
+        else if (isMixed)
+        {
+            rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+        }
+    }
+
+    private void HandleShakeLogic()
+    {
+        Vector2 currentMousePos = Mouse.current.position.ReadValue();
+        float mouseDelta = Vector2.Distance(currentMousePos, lastMousePos);
+
+        if (mouseDelta > 0)
+        {
+            currentShakeProgress += mouseDelta;
+            if (shakeProgressBar != null)
+            {
+                shakeProgressBar.gameObject.SetActive(true);
+                shakeProgressBar.value = currentShakeProgress / shakeRequired;
+            }
+
+            // Visual wiggle: move slightly away from original position and back
+            rectTransform.anchoredPosition = originalPosition + new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f));
+        }
+
+        lastMousePos = currentMousePos;
+
+        if (currentShakeProgress >= shakeRequired)
+        {
+            FinishMixing();
+        }
+    }
+
+    private void FinishMixing()
+    {
+        isMixed = true;
+        rectTransform.anchoredPosition = originalPosition; // Snap back to center
+        if (shakeProgressBar != null) shakeProgressBar.gameObject.SetActive(false);
+        Debug.Log("Mixing Complete! Now drag to the glass.");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+
+        // If it's not dropped on a glass, return to its slot
+        if (!isMixed || transform.parent == canvas.transform)
+        {
+            rectTransform.anchoredPosition = originalPosition;
+        }
+    }
+
+    // This handles the bottles being dropped INTO the mixer
     public void OnDrop(PointerEventData eventData)
     {
-        if (contents.Count <MaxDrinks && !isMixed)
+        if (contents.Count < maxCapacity && !isMixed)
         {
             DragNDrop bottle = eventData.pointerDrag.GetComponent<DragNDrop>();
             if (bottle != null)
             {
                 contents.Add(bottle.bottleType);
-                Debug.Log($"Added {bottle.bottleType}. Total: {contents.Count}");
                 Destroy(eventData.pointerDrag);
-                if (contents.Count == MaxDrinks)
-                {
-                    Debug.Log("Mixer Full");
-                }
+                Debug.Log("Added: " + bottle.bottleType);
             }
         }
     }
-
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        throw new System.NotImplementedException();
-    }
-
 }
